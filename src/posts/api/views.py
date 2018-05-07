3dbf16b4431687dcaed03db9ba964be770f6a38e
Ashwin Dhakal <ashwindhakal97@gmail.com>
2018-05-07 17:14:00 +0545

    search implemented based on query

--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -1,3 +1,9 @@
+from django.db.models import Q
+
+from rest_framework.filters import (
+    SearchFilter,
+    OrderingFilter
+)
 from rest_framework.generics import (
     ListAPIView,
     CreateAPIView,
@@ -52,8 +58,22 @@ class PostDeleteAPIView(DestroyAPIView):
     serializer_class = PostDetailSerializer
 
 class PostListAPIView(ListAPIView):
-    queryset = Post.objects.all()
     serializer_class = PostListSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'content', 'user__first_name']
+
+    def get_queryset(self, *args, **kwargs):
+        queryset_list = Post.objects.all()
+        query = self.request.GET.get("q")
+        if query:
+            queryset_list = queryset_list.filter(
+                Q(title__icontains=query) |
+                Q(content__icontains=query) |
+                Q(user__first_name__icontains=query) |
+                Q(user__last_name__icontains=query)
+            ).distinct()
+        return queryset_list
+
 
 
 
